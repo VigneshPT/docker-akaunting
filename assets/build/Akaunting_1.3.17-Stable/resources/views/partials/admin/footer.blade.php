96e5c14ef89eb5d3c4b9f9d4174e8c9550f3cca1
sathishdeveloper25 <sathishdeveloper25@gmail.com>
2020-02-08 00:27:06 +0530

    code arrangement

--- assets/build/Akaunting_1.3.17-Stable/resources/views/partials/admin/footer.blade.php
+++ assets/build/Akaunting_1.3.17-Stable/resources/views/partials/admin/footer.blade.php
@@ -6,5 +6,5 @@
     </div> -->
     <!-- <strong>{{ trans('footer.powered') }}</strong>: <a href="{{ trans('footer.link') }}" target="_blank">{{ trans('footer.software') }}</a> -->
 </footer>
-
+ 
 @stack('footer_end')
