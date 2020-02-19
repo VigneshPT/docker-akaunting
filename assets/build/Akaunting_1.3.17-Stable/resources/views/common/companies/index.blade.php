6690a3363f3487433c7d94ce35712e0fcd2092eb
sathishdeveloper25 <sathishdeveloper25@gmail.com>
2020-02-20 03:10:38 +0530

    company date fix

--- assets/build/Akaunting_1.3.17-Stable/resources/views/common/companies/index.blade.php
+++ assets/build/Akaunting_1.3.17-Stable/resources/views/common/companies/index.blade.php
@@ -48,7 +48,7 @@
                         <td><a href="{{ url('common/companies/' . $item->id . '/edit') }}">{{ $item->company_name }}</a></td>
                         <td class="hidden-xs">{{ $item->domain }}</td>
                         <td class="hidden-xs">{{ $item->company_email }}</td>
-                        <td class="hidden-xs">{{ Date::parse($item->created_at)->format('Y-m-d') }}</td>
+                        <td class="hidden-xs">{{ Date::parse($item->created_at)->format('j M Y') }}</td>
                         <td class="hidden-xs">
                             @if ($item->enabled)
                                 <span class="label label-success">{{ trans('general.enabled') }}</span>
